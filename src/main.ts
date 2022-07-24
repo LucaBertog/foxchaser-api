a14dadd53d9f0816af9ea2a1391792765492351f
Xar0P <pietroricardocres@hotmail.com>
2022-07-24 12:37:59 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3000', 'https://foxchaser.netlify.app'],
+    },
+  });
   app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
