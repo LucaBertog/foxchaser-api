399c4fec8e6c35744a32cc847a80d6a618444db3
Xar0P <pietroricardocres@hotmail.com>
2022-07-29 11:19:06 -0300

    users controller

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  async getUserById(@Param('id') id: string) {
+    const response: any = await this.usersService.findById(id);
+    return {
+      statusCode: HttpStatus.OK,
+      user: {
+        name: response._doc.name,
+        username: response._doc.username,
+        profilePicture: response._doc.profilePicture,
+        coverPicture: response._doc.coverPicture,
+        followers: response._doc.followers,
+        followings: response._doc.followings,
+        description: response._doc.description,
+        emblems: response._doc.emblems,
+      },
+    };
+  }
+}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Exceptions } from 'src/common/utils/errors/exceptions.util';
 import { UserSchema } from 'src/models/users/entities/user.schema';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UsersService, Exceptions],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
