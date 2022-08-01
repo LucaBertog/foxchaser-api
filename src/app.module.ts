462c41669f53d1f68aed230d5847e60156addf85
Xar0P <pietroricardocres@hotmail.com>
2022-08-01 14:17:24 -0300

    test3

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,19 @@ import { UsersModule } from './modules/users/users.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { ProfileModule } from './modules/profile/profile.module';
 
-console.log(process.env.MONGODB_URI);
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `${process.cwd()}/src/common/envs/.env`,
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: process.env.MONGODB_URI || config.get<string>('MONGODB_URI'),
+      }),
+    }),
     AuthModule,
     UsersModule,
     PostsModule,
